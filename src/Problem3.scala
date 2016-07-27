2638619b4b637a83b00d18ae7e7fae8003fe6a78
vkodali <vkodali@cloudphysics.com>
2016-07-27 12:36:36 -0700

    Problem 3 modified

--- src/Problem3.scala
+++ src/Problem3.scala
@@ -3,6 +3,6 @@ object Problem3 {
     def factors(n: Long) = {
       (2 to math.sqrt(n).toInt).filter(n % _ ==0)
     }
-    factors(600851475143L).filter(i => !(2 to (i - 1)).exists(x => i % x == 0)).max
+    factors(600851475143L).filter(e => !(2 to (e - 1)).exists(x => e % x == 0)).max
   }
 }
